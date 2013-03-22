8804f9121c3bc89ddbebc1de81d9e0505c88a7d6
Kanak Biscuitwala <kanak.b@hotmail.com>
2013-03-21 22:23:59 -0700

    Get notified on app installs

---
+++ src/mobisocial/rectacular/services/AppInstallReceiver.java
@@ -0,0 +1,20 @@
+package mobisocial.rectacular.services;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class AppInstallReceiver extends BroadcastReceiver {
+    private static final String TAG = "AppInstallReceiver";
+    
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent == null || intent.getData() == null) {
+            Log.d(TAG, "no data");
+            return;
+        }
+        Log.d(TAG, "uri: " + intent.getData().toString());
+    }
+
+}
